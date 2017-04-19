c528abe30414154468abb9b84db27d535afeeef6
zfy0098 <zfy0098@163.com>
2017-04-19 10:44:19 +0800

    删除敏感信息

--- src/main/java/com/example/constant/Constant.java
+++ src/main/java/com/example/constant/Constant.java
@@ -1,16 +1,7 @@
 package com.example.constant;
 
 public class Constant {
-	public static final String AppID = "wx18265cb5b152a10b";
-	
-	public static final String AppSecret = "b9ef383a17addc3f5f58075777766f84";
-	
-	public static final String TOKEN = "zhoufangyuweixin";
-	
-	public static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+ AppID + "&secret=" + AppSecret;
 
-	public static final String CREATE_MEUN_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token="; 
-	
 
 }
 
